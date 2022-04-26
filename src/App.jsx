062d9ac00544d0f57730e90229178b1c03a4d8a9
Carmen Carrion <carmencarrioncar@gmail.com>
2022-04-26 11:40:04 +0200

    primer comentario

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { TodoList } from './components/TodoList';
+
+export function App(){
+
+   return  <TodoList todos={[
+       {id: 1, task: 'Tarea 1'}
+   ]} />
+}
