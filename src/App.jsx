24a7b39074deb2a1643dc6a451498e4f1c9c19bf
Carmen Carrion <carmencarrioncar@gmail.com>
2022-05-03 13:07:12 +0200

    borrar pruebas anteriores

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,6 @@ import React from "react";
 import GlobalStyle from './globalStyle';
 import { TodoList } from './components/TodoList';
 
-export function App(){
-
-   return  <TodoList todos={[
-       {id: 1, task: 'Tarea 1'}
-   ]} />
-}
 
 
 export default App;
